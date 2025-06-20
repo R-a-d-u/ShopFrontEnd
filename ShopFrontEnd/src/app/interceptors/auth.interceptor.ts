6054c9dc76f2f34ff3b87a891c46c8483fc93cec
radu cruceat <radu.cruceat@gmail.com>
2025-06-20 18:03:16 +0300

    jwt change

--- ShopFrontEnd/src/app/interceptors/auth.interceptor.ts
+++ ShopFrontEnd/src/app/interceptors/auth.interceptor.ts
@@ -19,17 +19,20 @@ export class AuthInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const token = this.authService.token;
     
-    if (token && this.authService.isAuthenticated()) {
+   
+     if (token) {
       request = request.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`
         }
       });
     }
+    
 
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
-        if (error.status === 401) {
+         if (error.status === 401 || error.status === 403) {
+          console.warn('Authentication failed, logging out user');
           this.authService.logout();
         }
         return throwError(() => error);
